8fe6cf901b58a8a133a187f1a324bf4524d86060
Dean Wild <dean.wild@commscope.com>
2020-12-14 01:20:24 +0000

    day 4 part 2 solution

--- day4/day4.go
+++ day4/day4.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -34,18 +35,101 @@ func getNumberOfValidPassports(inputSource string) int {
 	validCount := 0
 
 	for _, passport := range passports {
-		if passport.BirthYear != "" &&
-			passport.IssueYear != "" &&
-			passport.ExpirationYear != "" &&
-			passport.Height != "" &&
-			passport.HairColor != "" &&
-			passport.EyeColor != "" &&
-			passport.PassportID != "" {
+		if isPassportValid(passport) {
 			validCount++
 		}
 	}
 
 	return validCount
+}
+
+func isPassportValid(passport Passport) bool {
+	return isBirthYearValid(passport.BirthYear) &&
+		isIssueYearValid(passport.IssueYear) &&
+		isExpirationYearValid(passport.ExpirationYear) &&
+		isHeightValid(passport.Height) &&
+		isHairColorValid(passport.HairColor) &&
+		isEyeColorValid(passport.EyeColor) &&
+		isPassportIDValid(passport.PassportID)
+}
+
+func isBirthYearValid(birthYear string) bool {
+	return validateNumber(birthYear, 1920, 2002)
+}
+
+func isIssueYearValid(birthYear string) bool {
+	return validateNumber(birthYear, 2010, 2020)
+}
+
+func isExpirationYearValid(birthYear string) bool {
+	return validateNumber(birthYear, 2020, 2030)
+}
+
+func isHeightValid(height string) bool {
+
+	if strings.HasSuffix(height, "cm") {
+		height = strings.Replace(height, "cm", "", 1)
+		return validateNumber(height, 150, 193)
+	} else if strings.HasSuffix(height, "in") {
+		height = strings.Replace(height, "in", "", 1)
+		return validateNumber(height, 59, 76)
+	} else {
+		return false
+	}
+}
+
+func validateNumber(numberStr string, min int, max int) bool {
+	if i, err := strconv.Atoi(numberStr); err == nil {
+		return (i >= min && i <= max)
+	}
+
+	return false
+}
+
+func isHairColorValid(hairColor string) bool {
+	if strings.HasPrefix(hairColor, "#") {
+		hairColor = strings.Replace(hairColor, "#", "", 1)
+		if len(hairColor) == 6 {
+			if i, err := strconv.ParseUint(hairColor, 16, 64); err == nil {
+				return (i >= 0 && i <= 16777215)
+			}
+
+		}
+	}
+
+	return false
+}
+
+func isEyeColorValid(eyeColor string) bool {
+
+	isValid := false
+
+	switch eyeColor {
+	case "amb":
+		fallthrough
+	case "blu":
+		fallthrough
+	case "brn":
+		fallthrough
+	case "gry":
+		fallthrough
+	case "grn":
+		fallthrough
+	case "hzl":
+		fallthrough
+	case "oth":
+		isValid = true
+	}
+
+	return isValid
+}
+
+func isPassportIDValid(passportID string) bool {
+	if len(passportID) == 9 {
+		return true
+	}
+
+	return false
 }
 
 func parsePassports(inputSource string) []Passport {
--- day4/day4_test.go
+++ day4/day4_test.go
@@ -4,12 +4,31 @@ import "testing"
 
 func TestSampleInput(t *testing.T) {
 
+	// TEST sample.txt
 	expected := 4
 	passports := parsePassports("sample.txt")
 	checkValue(len(passports), expected, t)
 
-	expected = 2
+	expected = 0
 	actual := getNumberOfValidPassports("sample.txt")
+	checkValue(actual, expected, t)
+
+	// TEST invalid_sample.txt
+	expected = 4
+	passports = parsePassports("invalid_sample.txt")
+	checkValue(len(passports), expected, t)
+
+	expected = 0
+	actual = getNumberOfValidPassports("invalid_sample.txt")
+	checkValue(actual, expected, t)
+
+	// TEST valid_sample.txt
+	expected = 4
+	passports = parsePassports("valid_sample.txt")
+	checkValue(len(passports), expected, t)
+
+	expected = 0
+	actual = getNumberOfValidPassports("valid_sample.txt")
 	checkValue(actual, expected, t)
 
 }
